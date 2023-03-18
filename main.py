459ab603948287001e61f9f0e599a546a6db2479
Krzysztof Romanczuk <105151954+KrzysztofRomanczuk@users.noreply.github.com>
2023-03-18 12:31:42 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import cv2 as cv
-import numpy as np
-import matplotlib as plt
-from tensorflow import keras, layers
-
-(training_images, training_labels), (testing_images, testing_labels) = keras.datasets.cifar10.load_data()
-training_images, testing_images / training_images / 255, testing_images / 255
-
